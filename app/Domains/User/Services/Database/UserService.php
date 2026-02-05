0454217b3255fca81271afd30ab62b86cc458430
AhmedElazony <ahmedazony2@gmail.com>
2026-02-05 22:12:23 +0200

    fix: user service not using the crud BaseService.

--- app/Domains/User/Services/Database/UserService.php
+++ app/Domains/User/Services/Database/UserService.php
@@ -2,9 +2,14 @@
 
 namespace App\Domains\User\Services\Database;
 
-use App\Domains\User\Services\Contracts\UserServiceInterface;
+use App\Domains\User\Models\User;
+use App\Domains\User\Services\Contracts\UserService as UserServiceContract;
+use App\Support\Services\Database\BaseService;
 
-class UserService implements UserServiceInterface
+class UserService extends BaseService implements UserServiceContract
 {
-    //
+    public function __construct()
+    {
+        parent::__construct(User::class);
+    }
 }
