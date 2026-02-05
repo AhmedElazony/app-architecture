d7644ac12b5e886f4f3905448cf2f9d12b524e15
AhmedElazony <ahmedazony2@gmail.com>
2026-02-05 22:04:38 +0200

    feat: add HasFilters trait for searching in queries with Eloquent Scope.

--- app/Domains/User/Models/User.php
+++ app/Domains/User/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Domains\User\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Support\Traits\HasFilters;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasFilters, Notifiable;
 
     /**
      * The attributes that are mass assignable.
